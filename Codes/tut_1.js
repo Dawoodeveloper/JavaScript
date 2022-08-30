924fa0176692dc7c989128dc2433416021c053a0
Dawood Sarfraz Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2022-08-30 12:50:24 -0700

    initial commit

---
+++ Codes/tut_1.js
@@ -0,0 +1,21 @@
+console.time('Your code Took');
+console.log('welcome JavaScript');
+console.log(456564/364564);
+console.log(34)*4646;
+console.log(true);
+console.log([34355,626656,1577,2756]);
+console.log({Name: 'Dawood', marks : 1100});
+console.table({Name: 'Dawood', marks : 1100});
+console.warn('This is a warning');
+// console.clear();
+console.timeEnd('Your code Took');
+console.error("This shows errors:::")
+console.assert(566<189, 'Age >189 is not possible')
+// console.error('This is an error')
+
+/*
+this
+is a
+multiline comment
+*/
+
